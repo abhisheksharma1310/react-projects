f355bdae28160313038364b10683adb9fb0b330a
abhisheksharma <abhisheksharma131098@gmail.com>
2024-03-06 18:12:56 +0530

    Load more image component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordion from "./components/accordian";
 import ImageSlider from "./components/image-slider";
+import LoadMoreImage from "./components/load-more-image";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -10,11 +11,12 @@ function App() {
       {/* <Accordion/> */}
       {/* <RandomColor /> */}
       {/* <StarRating/> */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+      <LoadMoreImage/>
     </div>
   );
 }
